393c3cf35dd6ecc51917a42b314133be06e81676
Maria Mora <mariamora2807@gmail.com>
2022-04-07 18:29:14 -0500

    fix base url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://mariamorav.github.io/',
+  base: 'https://mariamorav.github.io/mm-react-vite/',
   plugins: [react()]
 })
